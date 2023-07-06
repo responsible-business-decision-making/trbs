5ba7f447ba1769232b3928af4a848a51ea011ce3
Thom-Ivar van Dijk <thom-ivar.van.dijk@pwc.com>
2023-07-06 16:52:38 +0200

    add operator to evaluate

--- model/core/evaluate.py
+++ model/core/evaluate.py
@@ -4,25 +4,27 @@ options.
 """
 
 
-def evaluate(
-    argument_1_value: str,
-    argument_2_value: str,
-):
+def evaluate(argument_1_value: str, argument_2_value: str, operator: str):
     """
     This function ...
     """
+    # dictionary containing all functions to which operators are related
+    operators_dict = {"-": lambda x, y: x - y, "+": lambda x, y: x + y, "*": lambda x, y: x * y}
 
-    return argument_1_value + argument_2_value
+    # apply operations based on occurence in operators_dict
+    if operator in operators_dict.keys():  # ignore warning about .keys() | pylint: disable=C0201
+        return operators_dict[operator](argument_1_value, argument_2_value)
+
+    return f"{operator} not available"
 
 
 if __name__ == "__main__":
-    from pathlib import Path
-    from import_case import import_case
+    # from pathlib import Path
+    # from import_case import import_case
+    #
+    # file_format = "xlsx"
+    # path = Path.cwd() / "model/data" / "beerwiser"
+    # case_data = import_case(file_format, path)
 
-    file_format = "xlsx"
-    path = Path.cwd() / "model/data" / "beerwiser"
-    case_data = import_case(file_format, path)
-
-    print(case_data)
-
-    evaluate(1, 2)
+    print(evaluate(1, 2, "+"))
+    print(evaluate(3, 4, "*"))
