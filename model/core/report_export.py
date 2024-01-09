8a9b8e359908709dc5897281b289d7cc3fe9ff76
MikeVerdaasdonk297 <mike.verdaasdonk@pwc.com>
2024-01-09 10:15:48 +0100

    adjust feedback Christian

--- model/core/report_export.py
+++ model/core/report_export.py
@@ -17,6 +17,38 @@ from pptx.enum.chart import XL_LEGEND_POSITION
 import numpy as np
 
 
+def make_subtitle(slide) -> None:
+    """
+    This function adds a subtitle to a slide
+    :param slide: selected slide where the title is needed
+    """
+    date_today = str(date.today().strftime("%d %B %Y"))
+    subtitle = slide.placeholders[1]
+    subtitle.top, subtitle.left, subtitle.width, subtitle.height = Cm(10.8), Cm(7.6), Cm(17.8), Cm(4.9)
+    subtitle.text = f"Responsible business decision making \n {date_today}"
+
+
+def iter_cells(table) -> None:
+    """
+    This function makes it possible to iterate over all cells in a table
+    :param table: selected table
+    """
+    for row in table.rows:
+        for cell in row.cells:
+            yield cell
+
+
+def change_format_cells(table, format_size) -> None:
+    """
+    This function makes it possible to change the format of all cells in a table
+    :param table: selected table
+    :param format_size: the desired format size of the cells in a table
+    """
+    for cell in iter_cells(table):
+        for paragraph in cell.text_frame.paragraphs:
+            for run in paragraph.runs:
+                run.font.size = Pt(format_size)
+
 
 class ReportExporter:
     """
@@ -52,17 +84,17 @@ class ReportExporter:
             text_element = "generic_text_element"
         if "title_" + target in self.input_dict[text_element + "s"]:
             if str(
-                self.input_dict[text_element + "_value"][
-                    list(self.input_dict[text_element + "s"]).index("title_" + target)
-                ]
-            ) != str("nan"):
-                title.text = (
                     self.input_dict[text_element + "_value"][
                         list(self.input_dict[text_element + "s"]).index("title_" + target)
                     ]
-                    + scenario
-                    + pos_series
-                    + key_output
+            ) != str("nan"):
+                title.text = (
+                        self.input_dict[text_element + "_value"][
+                            list(self.input_dict[text_element + "s"]).index("title_" + target)
+                        ]
+                        + scenario
+                        + pos_series
+                        + key_output
                 )
             else:
                 title.text = "Not defined in template"
@@ -73,16 +105,6 @@ class ReportExporter:
             title.text_frame.paragraphs[0].runs[0].font.italic = True
             title.text_frame.paragraphs[0].runs[0].font.color.rgb = RGBColor(255, 0, 0)
         slide.shapes.title.text_frame.paragraphs[0].font.size = Pt(36)
-
-    def make_subtitle(self, slide) -> None:
-        """
-        This function adds a subtitle to a slide
-        :param slide: selected slide where the title is needed
-        """
-        date_today = str(date.today().strftime("%d %B %Y"))
-        subtitle = slide.placeholders[1]
-        subtitle.top, subtitle.left, subtitle.width, subtitle.height = Cm(10.8), Cm(7.6), Cm(17.8), Cm(4.9)
-        subtitle.text = f"Responsible business decision making \n {date_today}"
 
     def make_introduction(self, slide, target) -> None:
         """
@@ -99,9 +121,9 @@ class ReportExporter:
             text_element = "generic_text_element"
         if "intro_" + target in self.input_dict[text_element + "s"]:
             if str(
-                self.input_dict[text_element + "_value"][
-                    list(self.input_dict[text_element + "s"]).index("intro_" + target)
-                ]
+                    self.input_dict[text_element + "_value"][
+                        list(self.input_dict[text_element + "s"]).index("intro_" + target)
+                    ]
             ) != str("nan"):
                 text_frame.text = self.input_dict[text_element + "_value"][
                     list(self.input_dict[text_element + "s"]).index("intro_" + target)
@@ -124,9 +146,9 @@ class ReportExporter:
         tx_box.text_frame.word_wrap = True
         if "strategic_challenge" in self.input_dict["case_text_elements"]:
             if str(
-                self.input_dict["case_text_element_value"][
-                    list(self.input_dict["case_text_elements"]).index("strategic_challenge")
-                ]
+                    self.input_dict["case_text_element_value"][
+                        list(self.input_dict["case_text_elements"]).index("strategic_challenge")
+                    ]
             ) != str("nan"):
                 tx_box.text = self.input_dict["case_text_element_value"][
                     list(self.input_dict["case_text_elements"]).index("strategic_challenge")
@@ -142,17 +164,8 @@ class ReportExporter:
         tx_box.text_frame.paragraphs[0].font.size = Pt(28)
         tx_box.text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
 
-    def iter_cells(self, table) -> None:
-        """
-        This function makes it possible to iterate over all cells in a table
-        :param table: selected table
-        """
-        for row in table.rows:
-            for cell in row.cells:
-                yield cell
-
     def make_table_2_col(
-        self, slide, col_left, col_right, col_left_header, col_right_header, first_element=0, last_element=10
+            self, slide, col_left, col_right, col_left_header, col_right_header, first_element=0, last_element=10
     ) -> None:
         """
         This function makes it possible to iterate over all cells in a table
@@ -175,8 +188,12 @@ class ReportExporter:
         for column, table_name in enumerate([input_info_col_left, input_info_col_right]):
             for row in range(1, (len(input_info_col_left) + 1)):
                 cell = table.cell(row, column)
-                cell.text = str(table_name[row - 1])
-        self.change_format_cells(table, 12)
+                value = table_name[row - 1]
+                if isinstance(value, float):
+                    cell.text = '{:,.2f}'.format(value).replace(',', '*').replace('.', ',').replace('*', '.')
+                else:
+                    cell.text = value
+        change_format_cells(table, 12)
 
     def make_table_n_col(self, slide, col_names, col_values, row_names, left_col_header) -> None:
         """
@@ -208,8 +225,8 @@ class ReportExporter:
             for row in range(1, (len(input_info_row_values) + 1)):
                 cell = table.cell(row, column)
                 value = float(input_info_col_values[column - 1][row - 1])
-                cell.text = f"{value:.2f}"
-        self.change_format_cells(table, 12)
+                cell.text = '{:,.2f}'.format(value).replace(',', '*').replace('.', ',').replace('*', '.')
+        change_format_cells(table, 12)
 
     def make_figure(self, scenario, slide, results) -> None:
         """
@@ -241,11 +258,12 @@ class ReportExporter:
             fill = series.format.fill
             fill.solid()
             color_used = colors[index % 5].lstrip("#")
-            rgb = tuple(int(color_used[i : i + 2], 16) for i in (0, 2, 4))
+            rgb = tuple(int(color_used[i: i + 2], 16) for i in (0, 2, 4))
             fill.fore_color.rgb = RGBColor(rgb[0], rgb[1], rgb[2])
         #  layout chart change
         category_axis = chart.category_axis
         category_axis.tick_labels.font.size = Pt(9)
+        category_axis.tick_labels.font.bold = True
         value_axis = chart.value_axis
         value_axis.maximum_scale = 100
         value_axis.major_unit = 20
@@ -261,17 +279,6 @@ class ReportExporter:
         chart.legend.horz_offset = 0.0
         chart.legend.include_in_layout = False
         chart.legend.font.size = Pt(8)
-
-    def change_format_cells(self, table, format_size) -> None:
-        """
-        This function makes it possible to change the format of all cells in a table
-        :param table: selected table
-        :param format_size: the desired format size of the cells in a table
-        """
-        for cell in self.iter_cells(table):
-            for paragraph in cell.text_frame.paragraphs:
-                for run in paragraph.runs:
-                    run.font.size = Pt(format_size)
 
     def add_page_number(self, slide) -> int:
         """
@@ -306,7 +313,7 @@ class ReportExporter:
         # Generate title slide
         slide_title = prs.slides.add_slide(prs.slide_layouts[0])
         self.make_title(slide_title, "front_page", True)
-        self.make_subtitle(slide_title)
+        make_subtitle(slide_title)
 
         # Generate strategic challenge slide
         slide_challenge = prs.slides.add_slide(prs.slide_layouts[5])
@@ -372,7 +379,7 @@ class ReportExporter:
         os.mkdir(str(rand_dir))
         for key_output in self.output_dict[scenario][next(iter(self.output_dict[scenario]))]["key_outputs"].items():
             slide_dep_graph = prs.slides.add_slide(prs.slide_layouts[5])
-            self.make_title(slide_dep_graph, "dependency_graph", key_output=" for key output " + key_output[0])
+            self.make_title(slide_dep_graph, "dependency_graph", key_output=" for key output: " + key_output[0])
             self.make_introduction(slide_dep_graph, "dependency_graph")
             # slide_dep_graph.shapes.add_picture("dep-graph.png", Cm(3.62), Cm(6.2), Cm(25.5), Cm(12.5))
             self.page_number = self.add_page_number(slide_dep_graph)
